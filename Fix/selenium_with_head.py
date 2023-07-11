bbd78c00645468362bdd4d0cb2798637df52cfd6
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-07-11 09:11:30 +0200

    small fix

--- Fix/selenium_with_head.py
+++ Fix/selenium_with_head.py
@@ -10,6 +10,7 @@ total_number_of_url = 0
 output_data = []
 global _signals
 
+
 def file_to_list(filename):
     with open(filename, 'r') as file:
         lines = file.read().splitlines()
@@ -20,9 +21,20 @@ def load_user_agents():
     global user_agents
     user_agents = file_to_list("chrome_useragents.csv")
 
+
 class headed_scraper:
     def __init__(self):
+        self.driver = None
         load_user_agents()
+        self.reset_driver()
+        self.xpath_item_to_wait = "/html/body/main/nav"
+
+        time.sleep(5)
+
+    def set_xpath_wait(self, xpath):
+        self.xpath_item_to_wait = xpath
+
+    def reset_driver(self):
         options = uc.ChromeOptions()
         options.add_argument("user-agent=" + random.choice(user_agents))
 
@@ -30,15 +42,8 @@ class headed_scraper:
         options.add_argument('--load-extension=./buster')
 
         self.driver = uc.Chrome(options=options)
-        self.xpath_item_to_wait="/html/body/main/nav"
-
-        time.sleep(5)
-
-    def set_xpath_wait(self, xpath):
-        self.xpath_item_to_wait = xpath
 
     def get(self, url):
         self.driver.get(url)
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, self.xpath_item_to_wait)))
         return self.driver.page_source
-
